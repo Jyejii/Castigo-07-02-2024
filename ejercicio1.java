4d1834c3c3bbd2fa293c7bfeeb3ba90a14c7a5d1
Jiachen <jye@myuax.com>
2024-02-07 18:34:20 +0100

    1

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,3 +1,8 @@
-public class ejercicio1 {
-
+import java.util.Scanner;
+public class ejercicio1{
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Ingrese el numero de años");
+    }
 }
+
