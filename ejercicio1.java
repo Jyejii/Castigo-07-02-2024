51e860d88093c4716a9815a03b3a47c046999715
Jiachen <jye@myuax.com>
2024-02-07 18:45:11 +0100

    1

--- ejercicio1.java
+++ ejercicio1.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 public class ejercicio1{
     public static void main(String[]args){
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Ingrese el numero de años");
+        System.out.print("Ingrese el numero de años:");
+        int anos=scanner.nextInt();
+        long SegundosTotal=calcularSengundosEnAnos(anos);
+
+        System.out.println("el numero de segundos en "+anos+"años es:"+SegundosTotal);
+    }
+
+    public static long calcularSengundosEnAnos(int anos){
+        int diasEnAnos=anos*365;
+        int horasEnDias=24;
+        int minutosEnHoras=60;
+        int segundosEnMinutos=60;
+        long segundosEnAnos=(long) diasEnAnos*horasEnDias*minutosEnHoras*segundosEnMinutos;
+        return segundosEnAnos;
     }
 }
 
